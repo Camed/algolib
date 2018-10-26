e628564f3cf249f38ba8154c8161b67524c881eb
Gabriel <camed.tf@gmail.com>
2018-10-26 15:18:11 +0200

    BubbleSort + UnitTests
    Fixed problems with <T> and IComparable

---
+++ AlgoLib/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoLib
+{
+    public class Node<T> where T : IComparable
+    {
+        public T Value { get; set; }
+       
+        public int CompareTo(Node<T> node)
+        {
+            return this.Value.CompareTo(node.Value);
+        }
+
+        public Node()
+        {
+            Value = default(T);
+        }
+
+        public Node(T x)
+        {
+            Value = x;
+        }
+    }
+}
---
+++ AlgoLib/Sorting.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoLib
+{
+    public abstract class Sorting<T> where T : IComparable
+    { 
+        abstract public void Swap(ref Node<T> n, ref Node<T> m);
+        abstract public Node<T>[] Sort(Node<T>[] nodes);
+        abstract public Node<T>[] ReverseSort(Node<T>[] nodes);
+    }
+
+    public class BubbleSort<T> : Sorting<T> where T : IComparable
+    {
+        public override void Swap(ref Node<T> n, ref Node<T> m)
+        {
+            Node<T> temp = n;
+            n = m;
+            m = temp;
+        }
+
+        public override Node<T>[] Sort(Node<T>[] nodes)
+        {
+            for(int i = 0; i < nodes.Length; i++)
+            {
+                for(int j = 0; j < nodes.Length - i - 1; j++)
+                {
+                    if(nodes[j].CompareTo(nodes[j+1]) > 0)
+                    {
+                        Swap(ref nodes[j], ref nodes[j + 1]);
+                    }
+                }
+            }
+            return nodes;
+        }
+
+        public override Node<T>[] ReverseSort(Node<T>[] nodes)
+        {
+            Sort(nodes);
+            Array.Reverse(nodes);
+            return nodes;
+        }
+    }
+
+    public class SelectionSort<T> : Sorting<T> where T: IComparable
+    {
+        public override void Swap(ref Node<T> n, ref Node<T> m)
+        {
+            Node<T> temp = n;
+            n = m;
+            m = temp;
+        }
+        public override Node<T>[] Sort(Node<T>[] nodes)
+        {
+            return nodes;
+        }
+        public override Node<T>[] ReverseSort(Node<T>[] nodes)
+        {
+            return nodes;
+        }
+    }
+}
